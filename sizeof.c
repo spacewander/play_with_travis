702f0a63b74d74f79ad8904072fc2bdf812e68cf
spacewander <spacewanderlzx@gmail.com>
2018-11-26 10:39:14 +0800

    more

--- sizeof.c
+++ sizeof.c
@@ -2,8 +2,10 @@
 
 int main(int argc, char *argv[])
 {
+#if !defined(_WIN64)
     printf("sizeof ptr %lu\n", sizeof(void *));
     printf("sizeof unsigned long long %lu\n", sizeof(1ULL));
+#endif
     printf("%llu\n", 1ULL << 47);
     return 0;
 }
